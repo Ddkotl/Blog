f9c1fff5d2d1e8d1a22352571771eb0d812e4d5b
Ddkotl <no64129@gmail.com>
2024-02-20 21:30:13 +0300

    add prisma and docker compose

---
+++ prisma/migrations/20240220181441_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Novel" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Novel_pkey" PRIMARY KEY ("id")
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/shared/ui/button";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+const client = new PrismaClient();
+
+export default async function Home() {
+  const novels = await client.novel.findMany();
+  console.log(novels);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Button</Button>
