cebd649c14dc6ac26e2c0e77bc89d7f0be5e95b3
Ddkotl <no64129@gmail.com>
2024-02-22 22:06:54 +0300

    ci fix jest

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: "v8",
+  modulePathIgnorePatterns: ["<rootDir>/tests/"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     // ...
