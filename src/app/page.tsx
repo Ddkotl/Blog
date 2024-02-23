2623f1209fa38e847bf2868382b07acb9a79aa1f
Ddkotl <no64129@gmail.com>
2024-02-23 20:49:43 +0300

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { NovelsList } from "@/features/novels-list/pub/novels-list";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
+      <h1>auto-deploy -success</h1>
       <CreateNovelForm revalidatePagePath="/" className="max-w-[300px] mb-10" />
       <NovelsList revalidatePagePath="/" />
     </main>
