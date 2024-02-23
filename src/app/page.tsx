fc79e750b8a790a3be62746ff3e2c7962699f80c
Ddkotl <no64129@gmail.com>
2024-02-23 21:23:57 +0300

    page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
       <h1>Novels</h1>
+      <h2>Novels</h2>
+      <h3>Novels</h3>
       <CreateNovelForm revalidatePagePath="/" className="max-w-[300px] mb-10" />
       <NovelsList revalidatePagePath="/" />
     </main>
