82987dcd48192bfc9efedc63e83e5d463837f541
Ddkotl <no64129@gmail.com>
2024-03-07 20:07:22 +0300

    add base auth

--- src/app/_providers/app-provider.tsx
+++ src/app/_providers/app-provider.tsx
@@ -1,6 +1,9 @@
 "use client";
+import { AppSessionProvider } from "@/entities/session/app-session-provider";
 import { ThemeProvider } from "@/features/theme/theme.provider";
+import { queryClient } from "@/shared/api/query-client";
 import { ComposeChildren } from "@/shared/lib/react";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +14,8 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
         enableSystem
         disableTransitionOnChange
       />
+      <AppSessionProvider />
+      <QueryClientProvider client={queryClient} />
       {children}
     </ComposeChildren>
   );
