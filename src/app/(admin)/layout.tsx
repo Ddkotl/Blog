567d22708bdf0d1584256dcda84580b3a58a1382
Ddkotl <no64129@gmail.com>
2024-04-09 21:15:44 +0300

    fix

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,11 +1,6 @@
-import AdminGuard from "@/features/admin/admin-guard";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "@/shared/ui/navigation-menu";
-import { AppHeader } from "@/widgets/app-header/app-header";
+import AdminGuard from "@/features/admin/admin-guard"
+import { AppHeader } from "@/widgets/app-header/app-header"
+import Link from "next/link"
 
 export default async function Layout({
   children,
@@ -16,24 +11,22 @@ export default async function Layout({
     <>
       <AppHeader variant="admin" />
       <AdminGuard>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/admin">dashbord</NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/admin/post">post</NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/admin/user">user</NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/admin/statistics">
-                statistics
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className="sticky px-4 top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+          <div className="container flex h-14 items-center">
+            <div className="mr-4  flex ">
+              <Link href="/admin">dashbord</Link>
+            </div>
+            <div className="mr-4  flex ">
+              <Link href="/admin/post">post</Link>
+            </div>
+            <div className="mr-4  flex ">
+              <Link href="/admin/user">user</Link>
+            </div>
+            <div className="mr-4  flex ">
+              <Link href="/admin/statistics">statistics</Link>
+            </div>
+          </div>
+        </div>
         {children}
       </AdminGuard>
     </>
