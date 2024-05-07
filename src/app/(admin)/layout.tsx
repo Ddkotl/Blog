2bf9bfbd5a9e769f784f97e3a9760287c72ab8b8
Ddkotl <no64129@gmail.com>
2024-05-07 18:57:03 +0300

    update mobile admin nav

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -6,23 +6,11 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@/shared/ui/breadcrumb";
-import { Button } from "@/shared/ui/button";
 import { Input } from "@/shared/ui/input";
-import { Sheet, SheetContent, SheetTrigger } from "@/shared/ui/sheet";
+import { AdminNavMob } from "@/widgets/admin/admin-nav/admin-nav-mob";
 import { AdminSidebarPK } from "@/widgets/admin/admin-nav/admin-sidebar-pk";
 import { AppHeader } from "@/widgets/app-header/app-header";
-import {
-  BookCheck,
-  BookKey,
-  Home,
-  LineChart,
-  MessageCircle,
-  PanelLeft,
-  Search,
-  Settings,
-  Tag,
-  Users2,
-} from "lucide-react";
+import { Search } from "lucide-react";
 import Link from "next/link";
 
 export default async function Layout({
@@ -38,75 +26,7 @@ export default async function Layout({
           <AdminSidebarPK />
           <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
             <div className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button size="icon" variant="outline" className="sm:hidden">
-                    <PanelLeft className="h-5 w-5" />
-                    <span className="sr-only">Админ меню</span>
-                  </Button>
-                </SheetTrigger>
-                <SheetContent side="left" className="sm:max-w-xs">
-                  <nav className="grid gap-6 text-lg font-medium">
-                    <Link
-                      href="/admin"
-                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                    >
-                      <Home className="h-5 w-5" />
-                      Главная админки
-                    </Link>
-                    <Link
-                      href="/admin/post"
-                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                    >
-                      <BookCheck className="h-5 w-5" />
-                      Статьи
-                    </Link>
-                    <Link
-                      href="/admin/category"
-                      className="flex items-center gap-4 px-2.5  hover:text-foreground"
-                    >
-                      <BookKey className="h-5 w-5" />
-                      Категории
-                    </Link>
-                    <Link
-                      href="/admin/tag"
-                      className="flex items-center gap-4 px-2.5  hover:text-foreground"
-                    >
-                      <Tag className="h-5 w-5" />
-                      Тэги
-                    </Link>
-                    <Link
-                      href="/admin/user"
-                      className="flex items-center gap-4 px-2.5 text-foreground"
-                    >
-                      <Users2 className="h-5 w-5" />
-                      Пользователи
-                    </Link>
-                    <Link
-                      href="/admin/comment"
-                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                    >
-                      <MessageCircle className="h-5 w-5" />
-                      Комментарии
-                    </Link>
-
-                    <Link
-                      href="/admin/statistics"
-                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                    >
-                      <LineChart className="h-5 w-5" />
-                      Cтатистика
-                    </Link>
-                    <Link
-                      href="#"
-                      className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                    >
-                      <Settings className="h-5 w-5" />
-                      Настройки
-                    </Link>
-                  </nav>
-                </SheetContent>
-              </Sheet>
+              <AdminNavMob />
               <Breadcrumb className="hidden md:flex">
                 <BreadcrumbList>
                   <BreadcrumbItem>
--- src/widgets/admin/admin-nav/admin-nav-mob.tsx
+++ src/widgets/admin/admin-nav/admin-nav-mob.tsx
@@ -1,1 +1,148 @@
-export function AdminNavMob() {}
+"use client";
+import { Button } from "@/shared/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/shared/ui/sheet";
+import { cn } from "@/shared/ui/utils";
+import {
+  BookCheck,
+  BookKey,
+  Home,
+  LineChart,
+  MessageCircle,
+  PanelLeft,
+  Settings,
+  Tag,
+  Users2,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function AdminNavMob() {
+  const pathName = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline" className="sm:hidden">
+          <PanelLeft className="h-5 w-5" />
+          <span className="sr-only">Админ меню</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="sm:max-w-xs">
+        <nav className="grid gap-6 text-lg font-medium">
+          <Link
+            href="/admin"
+            className={cn(
+              `${
+                pathName === "/admin"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <Home className="h-5 w-5" />
+            Главная админки
+          </Link>
+          <Link
+            href="/admin/post"
+            className={cn(
+              `${
+                pathName === "/admin/post"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <BookCheck className="h-5 w-5" />
+            Статьи
+          </Link>
+          <Link
+            href="/admin/category"
+            className={cn(
+              `${
+                pathName === "/admin/category"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <BookKey className="h-5 w-5" />
+            Категории
+          </Link>
+          <Link
+            href="/admin/tag"
+            className={cn(
+              `${
+                pathName === "/admin/tag"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <Tag className="h-5 w-5" />
+            Тэги
+          </Link>
+          <Link
+            href="/admin/user"
+            className={cn(
+              `${
+                pathName === "/admin/user"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <Users2 className="h-5 w-5" />
+            Пользователи
+          </Link>
+          <Link
+            href="/admin/comment"
+            className={cn(
+              `${
+                pathName === "/admin/comment"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <MessageCircle className="h-5 w-5" />
+            Комментарии
+          </Link>
+
+          <Link
+            href="/admin/statistics"
+            className={cn(
+              `${
+                pathName === "/admin/statistics"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <LineChart className="h-5 w-5" />
+            Cтатистика
+          </Link>
+          <Link
+            href="/admin/settings"
+            className={cn(
+              `${
+                pathName === "/admin/settings"
+                  ? "bg-accent text-accent-foreground"
+                  : "text-muted-foreground"
+              }`,
+              "flex items-center gap-4 px-2.5  transition-colors hover:text-foreground ",
+            )}
+          >
+            <Settings className="h-5 w-5" />
+            Настройки
+          </Link>
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+}
